df62e2d4e8083bed98b0bdb86fa8fdcbdb0a721b
EdwinTomy <etomygeorg@miners.utep.edu>
2020-02-19 13:55:30 -0700

    Create class with attributes.

---
+++ Java/src/SnakeGame.java
@@ -0,0 +1,15 @@
+public class SnakeGame{
+    private boolean[][] game;
+    private int[] headPosition;
+    private static int exhaustiveChecks;
+    private static int recursiveChecks;
+
+    SnakeGame(){
+        game = new boolean[1][1];
+    }
+    SnakeGame(int[][] game, int x, int y){
+        for(int)
+    }
+    
+}
+
