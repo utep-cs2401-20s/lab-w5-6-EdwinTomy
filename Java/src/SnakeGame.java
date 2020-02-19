2a5ab80c50b4e45b5b2560faa83397b12a27ec07
EdwinTomy <etomygeorg@miners.utep.edu>
2020-02-19 14:48:55 -0700

    Constructors and findTailExhaustive

--- Java/src/SnakeGame.java
+++ Java/src/SnakeGame.java
@@ -4,12 +4,82 @@ public class SnakeGame{
     private static int exhaustiveChecks;
     private static int recursiveChecks;
 
+//Constructors
     SnakeGame(){
         game = new boolean[1][1];
     }
-    SnakeGame(int[][] game, int x, int y){
-        for(int)
+    SnakeGame(boolean[][] game, int x, int y){
+        for(int i = 0; i < game.length; i++){
+            for(int j = 0; j <game.length; j++){
+                this.game[i][j] = game[i][j];
+            }
+        }
+        headPosition = new int[] {x, y};
     }
-    
+
+//Methods
+    public int[] findTailExhaustive(){
+        resetCounters();
+        int[] tail = new int[3];
+        int counter = 0;
+
+        for(int i = 0; i < game.length; i++){
+            for(int j = 0; j <game.length; j++){
+                exhaustiveChecks++;
+                if(game[i][j]){
+                    counter++;
+
+                    if((neighbors(i,j) == 1) && (i != headPosition[0]) && (j != headPosition[1])){
+                        tail[0] = i;
+                        tail[1] = j;
+                    }
+                }
+            }
+        }tail[2] = counter;
+        return tail;
+    }
+
+    private void resetCounters(){
+        exhaustiveChecks = 0;
+        recursiveChecks = 0;
+    }
+
+    public int neighbors(int row, int column){
+        int sum = 0;
+
+        if(row != 0 && column != 0){
+            if(game[row - 1][column - 1])
+                sum++;
+        }
+        if(row != 0){
+            if(game[row - 1][column])
+                sum++;
+        }
+        if(row != 0 && column != game.length - 1){
+            if(game[row - 1][column + 1])
+                sum++;
+        }
+        if(column != 0){
+            if (game[row][column - 1])
+                sum++;
+        }
+        if(column != game.length - 1){
+            if(game[row][column + 1])
+                sum++;
+        }
+        if(row != game.length - 1 && column != 0){
+            if(game[row + 1][column - 1])
+                sum++;
+        }
+        if(row != game.length - 1){
+            if(game[row + 1][column])
+                sum++;
+        }
+        if(row != game.length - 1 && column != game.length - 1){
+            if(game[row + 1][column + 1])
+                sum++;
+        }
+        return sum;
+    }
 }
 
