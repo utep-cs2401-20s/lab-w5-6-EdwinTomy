2cf7e5674a32351bff4d5a00d543f5c96646a9c6
EdwinTomy <etomygeorg@miners.utep.edu>
2020-02-25 16:07:36 -0700

    Finished SnakeGame.java.

---
+++ Java/src/SnakeGameTester.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SnakeGameTester {
+    @Test
+    public void exhaustive1(){
+    }
+
+    public void Recursive(){
+    }
+}
